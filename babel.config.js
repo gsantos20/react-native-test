ace3efc101f0115fb14ab009cd6c440a4261a87d
Gustavo Neris <nerisgs20@gmail.com>
2023-09-18 02:27:42 -0300

    feat: add bundler to web and use expo router and config paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ['expo-router/babel',
+      ['module-resolver', {
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens'
+        }
+      }]
+    ],
+    ignore: [
+      '**/*.spec.ts'
+    ]
   };
 };
